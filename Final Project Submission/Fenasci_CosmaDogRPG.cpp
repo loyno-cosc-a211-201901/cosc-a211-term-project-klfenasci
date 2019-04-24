a5581c91415d787c02f473add34cd5a1cfa67ebb
klfenasci <46937837+klfenasci@users.noreply.github.com>
2019-04-24 11:54:27 -0500

    A - Comments

---
+++ Final Project Submission/Fenasci_CosmaDogRPG.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+//The program is an RPG game that is going to give the story of the first dog to go into space
+//The user will start by picking a character: either the dog, Laika, or a Russian scientist, Vladimir Yazdovsky
+//Either way, the user will go through the process of obtaining Laika (she a stray "rescued" from the streets of Moscow), training Laika to go into space,
+// and then sending her into space, in which she died in orbit; overall, the story will end by describing that this was the influence behind
+//the name of the band I'm in, Cosma Dog
+//Some of the calculations the program will have to do will be while training Laika, such as things like decibal noise and g force, etc.
+
+
+int main ()
+{
+}
